733e5d18359e5d6ac49b7fa6b176bfea78c53c67
BroBpony <vincentarrouays@hotmail.com>
2022-10-03 16:36:11 -0400

    fixed multiple imports

--- assignment-1/python/Assignment _ Poly Creation.py
+++ assignment-1/python/Assignment _ Poly Creation.py
@@ -1,15 +1,15 @@
 
 
 import argparse
+import maya.standalone
+import maya.cmds
+
 parser = argparse.ArgumentParser(description='This script creates a bunch of cylinders.')
 parser.add_argument('num_cubes', type=int, help="Number of cubes")
 args = parser.parse_args()
 
-import maya.standalone
-maya.standalone.initialize()
-import maya.cmds
 
-print("Creating {} cylinder(s)...".format(args.num_cylinders))
+print("Enter the number to Creating {} cylinder(s)...".format(args.num_cylinders))
 for i in range(args.num_cylinders):
     print("Created cylinders #{}".format(i))
     maya.cmds.polyCylinder()
@@ -17,4 +17,5 @@ for i in range(args.num_cylinders):
 print("Meshes in the Maya scene:")
 print(maya.cmds.ls(geometry=True))
 
-maya.standalone.initialize()
+
+maya.standalone.uninitialize()
