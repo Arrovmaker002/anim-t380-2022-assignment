82ab871b033ca74077931357c8e16e871ae1abc6
BroBpony <vincentarrouays@hotmail.com>
2022-10-02 21:44:44 -0400

    update to git

--- assignment-1/python/Assignment _ Poly Creation.py
+++ assignment-1/python/Assignment _ Poly Creation.py
@@ -1,17 +1,16 @@
-
 import argparse
-import maya.standalone
-maya.standalone.initialize()
-import maya.cmds
-
 parser = argparse.ArgumentParser(description='This script creates a bunch of cubes.')
 parser.add_argument('num_cubes', type=int, help="Number of cubes")
 args = parser.parse_args()
 
+import maya.standalone
+maya.standalone.initialize()
+import maya.cmds
+
 print("Creating {} cube(s)...".format(args.num_cubes))
 for i in range(args.num_cubes):
     print("Created cube #{}".format(i))
-    maya.cmds.polyCylinder()
+    maya.cmds.polyCube()
 
 print("Meshes in the Maya scene:")
 print(maya.cmds.ls(geometry=True))
