c9df8cda8cf91e6563caa920cb55dca0b1358903
BroBpony <vincentarrouays@hotmail.com>
2022-10-06 12:13:38 -0400

    Successfully added saving functionality

--- assignment-1/python/Assignment _ Poly Creation.py
+++ assignment-1/python/Assignment _ Poly Creation.py
@@ -3,7 +3,7 @@
 import argparse
 import maya.standalone
 maya.standalone.initialize()
-import maya.cmds
+import maya.cmds as cmds
 
 """This a docstring"""
 #this is a comment
@@ -23,8 +23,8 @@ print("Meshes in the Maya scene:")
 print(maya.cmds.ls(geometry=True))
 
 #save maya file as acii
-
-save
+cmds.file(rename="H:\Main Storage\College\Classes Current\Techincal Pipeline/anim-t380-2022-assignment/assignment-1\objects.ma")
+cmds.file(save=True, type="mayaAscii")
 
 
 #close maya in the background after process is done
