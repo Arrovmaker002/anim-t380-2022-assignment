d7344a6d02949ebbb530bb7614e453f22413b212
BroBpony <vincentarrouays@hotmail.com>
2022-10-10 00:04:50 -0400

    Update to Alias creation file

--- assignment - 3/python/Assignment _ 3_Aliase_file_.py
+++ assignment - 3/python/Assignment _ 3_Aliase_file_.py
@@ -4,7 +4,7 @@ import os
 '''-------------------------------- Create the .alias file ----------------------------------------'''
 
 #Create the file names and sets file location (file path to save to)
-filepath = os.path.join(r"H:\Main Storage\College\Classes Current\Techincal Pipeline\anim-t380-2022-assignment\assignment - 3\assets", "asset.alias")
+filepath = os.path.join(r"H:\Main Storage\College\Classes Current\Techincal Pipeline\anim-t380-2022-assignment\assignment - 3\etc", "asset.alias")
 
 #Creates the file named and loctation set above.
 alias = open(filepath, "w")
