919e4cffcfb95d658a02370fb0f7b47e3459125e
BroBpony <vincentarrouays@hotmail.com>
2022-10-27 12:50:21 -0400

    added frame range to test midterm on

--- Midterm/Midterm - naming and zipping frames.py
+++ Midterm/Midterm - naming and zipping frames.py
@@ -4,6 +4,8 @@
 #Import Packages
 import os
 import sys
+import shutil
+import argparse
 
 #dictionary for naming convention:
 namingdict = {
@@ -13,7 +15,13 @@ namingdict = {
 }
 
 
-
-#print out results to a line
+#print out dictionary results to a line
 savedFileName = namingdict
 print(savedFileName["project"]+"_"+savedFileName["file"]+"_"+savedFileName["frame"])
+
+
+
+#copying files in a directory using shutil
+
+source_folder = r"H:\Main Storage\College\Classes Current\Techincal Pipeline\anim-t380-2022-assignment\Midterm\asset\Techincal Pipeline Midterm\\"
+destination_folder = r"H:\Main Storage\College\Classes Current\Techincal Pipeline\anim-t380-2022-assignment\Midterm\asset\outgoing assets\\"
