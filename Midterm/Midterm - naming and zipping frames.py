426f7fcecee903ec49d4b5a6504eb2c0033917e7
BroBpony <vincentarrouays@hotmail.com>
2022-10-30 13:16:15 -0400

    Update Midterm - naming and zipping frames.py

--- Midterm/Midterm - naming and zipping frames.py
+++ Midterm/Midterm - naming and zipping frames.py
@@ -19,9 +19,24 @@ namingdict = {
 savedFileName = namingdict
 print(savedFileName["project"]+"_"+savedFileName["file"]+"_"+savedFileName["frame"])
 
-
-
 #copying files in a directory using shutil
 
-source_folder = r"H:\Main Storage\College\Classes Current\Techincal Pipeline\anim-t380-2022-assignment\Midterm\asset\Techincal Pipeline Midterm\\"
+source_folder = r"H:\Main Storage\College\Classes Current\Techincal Pipeline\anim-t380-2022-assignment\Midterm\asset\Techincal Pipeline Midterm\images\\"
 destination_folder = r"H:\Main Storage\College\Classes Current\Techincal Pipeline\anim-t380-2022-assignment\Midterm\asset\outgoing assets\\"
+
+
+"""
+# Function to rename multiple files
+def main():
+    folder = "xyz"
+    for count, filename in enumerate(os.listdir(folder)):
+        dst = f"Hostel {str(count)}.jpg"
+        src = f"{destination_folder}/{savedFileName}"  # foldername/filename, if .py file is outside folder
+        dst = f"{folder}/{dst}"
+
+        # rename() function will
+        # rename all the files
+        os.rename(src, dst)
+
+main()
+"""
