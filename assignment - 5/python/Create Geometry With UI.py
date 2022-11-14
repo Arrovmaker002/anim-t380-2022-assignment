534cc19e1d7ce0d0f29ec22366e6499dc0d3eb7d
BroBpony <vincentarrouays@hotmail.com>
2022-11-14 03:55:12 -0500

    update to assignment 5 and organization

---
+++ assignment - 5/python/Create Geometry With UI.py
@@ -0,0 +1,75 @@
+'''------------------------------Create A Cube with  random color------------------------------'''
+
+#import nessesary
+import os
+import bpy
+from bpy.types import (Panel, Operator)
+
+#This is for allowing the code to be used as an addon for Blender
+bl_info = {
+ "name": "Add Default Cube with random color",
+ "author": "Vincent Arrouays",
+ "description": "Creates a default Cube with a random color ",
+ "blender": (3, 30, 1),
+ "location": "3D View",
+ "warning": "",
+ "wiki_url": "",
+ "tracker_url": "",
+ "category": "Object"}
+
+#add cube mesh to scene operation
+class MeshOperator(Operator):
+
+#Operation name and label
+    bl_idname = "object.mesh_operator"
+    bl_label = "Generate New Cube"
+
+#execute it the function
+    def execute(self, context):
+
+        #command to add the cube
+        bpy.ops.mesh.primitive_cube_add(size=2, enter_editmode=False, align='WORLD', location=(0, 0, 0), scale=(1, 1, 1))
+
+        return {'FINISHED'}
+
+#add color attribute to cube mesh generated
+
+#UI code
+class QuickUI(bpy.types.Panel):
+    bl_idname = 'VIEW3D_PT_example_panel'
+    bl_label = 'Quick Transforms and UVs'
+    bl_space_type = 'VIEW_3D'
+    bl_region_type = 'UI'
+
+    # Create a a UI by drawing, and calling the classes of the functions
+    def draw(self, context):
+        layout = self.layout
+
+        col = layout.column(align=True)
+
+        # Get each operation that will be used for the panel
+        col.operator(TransformOperator.bl_idname, text="Generate Cube", icon="MESH_CUBE")
+
+
+
+
+
+#Register and Unregister the operations used in the panel (code from Brandon Jakovasic)
+
+classes = (MeshOperator, QuickUI)
+
+
+def register():
+ from bpy.utils import register_class
+ for cls in classes:
+  register_class(cls)
+
+
+def unregister():
+ from bpy.utils import unregister_class
+ for cls in reversed(classes):
+  unregister_class(cls)
+
+
+if __name__ == "__main__":
+ register()
