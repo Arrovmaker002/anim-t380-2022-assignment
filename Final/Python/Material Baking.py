6e29bdb14635634674c40bf4fb823f0dc477db47
BroBpony <vincentarrouays@hotmail.com>
2022-12-01 22:21:23 -0500

    Update to finals code
    adding code to register and unregister addons plus addon info data

--- Final/Python/Material Baking.py
+++ Final/Python/Material Baking.py
@@ -1,5 +1,40 @@
 '''----------------------------------------- Baking full pbr functionality -----------------------------------------'''
 
-
-
+# importing necessary packages
 import os
+import bpy
+from bpy.types import (Panel, Operator)
+
+# This is for allowing the code to be used as an addon for Blender
+bl_info = {
+    "name": "Simple PBR Baker",
+    "author": "Vincent Arrouays, Brandon Jakosavic",
+    "description": "Quickly bakes a material to  PBR texture set",
+    "blender": (3, 30, 1),
+    "location": "3D View",
+    "warning": "In Development",
+    "wiki_url": "",
+    "tracker_url": "",
+    "category": "Object"}
+
+
+
+
+
+
+# Register and Unregister the operations used in the panel (code from Brandon Jakovasic)
+
+classes = (MeshOperator, QuickUI)
+
+def register():
+    from bpy.utils import register_class
+    for cls in classes:
+        register_class(cls)
+
+def unregister():
+    from bpy.utils import unregister_class
+    for cls in reversed(classes):
+        unregister_class(cls)
+
+if __name__ == "__main__":
+    register()
