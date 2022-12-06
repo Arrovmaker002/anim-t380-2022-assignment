4f7653b1d7d675515e9c7816e0dab21942135fef
BroBpony <vincentarrouays@hotmail.com>
2022-12-05 20:35:45 -0500

    update to function testing for packing images

--- Final/Python/PBR to Unreal Packed function testing.py
+++ Final/Python/PBR to Unreal Packed function testing.py
@@ -2,45 +2,16 @@
 
 # importing necessary packages
 import os
+import numpy as np
+import matplotlib.pyplot as plt
+import cv2
 import bpy
 from bpy.types import (Panel, Operator)
 
-# This is for allowing the code to be used as an addon for Blender
-bl_info = {
-    "name": "PBR to Unreal Packed",
-    "author": "Vincent Arrouays, Brandon Jakosavic",
-    "description": "Converts a PBR texture set to a Unreal Packed Texture set within Blender",
-    "blender": (3, 30, 1),
-    "location": "3D View",
-    "warning": "In Development",
-    "wiki_url": "",
-    "tracker_url": "",
-    "category": "Object"}
+# load images to combine:
 
+matallicImg = cv2.imread
 
+OcclusionImg =
 
-# Register and Unregister the operations used in the panel (code from Brandon Jakovasic)
-
-classes = (BakeryOperator, QuickUI)
-
-class BakeryOperation(bpy.types.Operator):
-    bl_idname = "bakery.bake"
-    bl_label = "Convert Textures"
-
-    def invoke(self, context, event):
-        
-        return {'FINISHED'}
-
-
-def register():
-    from bpy.utils import register_class
-    for cls in classes:
-        register_class(cls)
-
-def unregister():
-    from bpy.utils import unregister_class
-    for cls in reversed(classes):
-        unregister_class(cls)
-
-if __name__ == "__main__":
-    register()
+RoughnessImg =
