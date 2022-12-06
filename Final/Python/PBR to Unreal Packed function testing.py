31c632b0cfe499272884c5423072d339e0baff6d
BroBpony <vincentarrouays@hotmail.com>
2022-12-05 20:54:40 -0500

    textures to test with

--- Final/Python/PBR to Unreal Packed function testing.py
+++ Final/Python/PBR to Unreal Packed function testing.py
@@ -5,13 +5,19 @@ import os
 import numpy as np
 import matplotlib.pyplot as plt
 import cv2
-import bpy
-from bpy.types import (Panel, Operator)
+
+#allow openCV to read .exr files: (may not be used due to it needing more conversions)
+
+os.environ["OPENCV_IO_ENABLE_OPENEXR"]="1"
+
+# For ease of testing:
+
+imgPath = "H:\Main Storage\College\Classes Current\Techincal Pipeline\anim-t380-2022-assignment\Final\Asset\Testing PBR to AOR"
 
 # load images to combine:
 
-matallicImg = cv2.imread
+matallicImg = cv2.imread( imgPath + "rust_coarse_01_ao_2k" , cv.anycolor )
 
-OcclusionImg =
+OcclusionImg = cv2.imread()
 
-RoughnessImg =
+RoughnessImg = cv2.imread()
