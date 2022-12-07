49599587a9bd7ae8f2759095df5dcce143128706
BroBpony <vincentarrouays@hotmail.com>
2022-12-07 03:34:23 -0500

    complete unreal packed converter code
    now to check for bugs

--- Final/Python/PBR to Packed Testing Addon.py
+++ Final/Python/PBR to Packed Testing Addon.py
@@ -6,10 +6,10 @@ from bpy.types import (Panel, Operator)
 
 # This is for allowing the code to be used as an addon for Blender
 bl_info = {
-    "name": "Simple PBR Baker",
+    "name": "PBR to Packed Converter",
     "author": "Vincent Arrouays, Brandon Jakosavic",
-    "description": "Quickly bakes a material to  PBR texture set",
-    "blender": (2, 80, 0),
+    "description": "Generates a setup ti combine AO, Mettalic and Roughness map into a single map for Unreal / other game engines",
+    "blender": (3, 30, 0), (2, 80, 0)
     "location": "3D View",
     "warning": "In Development",
     "wiki_url": "",
@@ -17,151 +17,76 @@ bl_info = {
     "category": "Object"}
 
 
-obj = bpy.context.active_object
-# Set a default name and Texture size
-image_name = obj.name + '_BakedTexture'
 
-
-img = bpy.data.images.new(image_name,1024,1024)
-
-#Since there are multiple material, we need to iterate on the materials to assign them to
-# the correct maps
-
-for mat in obj.data.materials:
-
-    mat.use_nodes = True
-    #Because this was made with node we say its true
-    nodes = mat.node_tree.nodes
-    texture_node =nodes.new('ShaderNodeTexImage')
-    texture_node.name = 'Bake_node'
-    texture_node.select = True
-    nodes.active = texture_node
-
-#Assign an image to the node
-
-    texture_node.image = img
 
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~PANELS~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
 
-#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ColorMap
+#----------------------------------Set up converter material and Plane
 
-#Define class for Recalculating Normals
-class BakeColorMapOperator(Operator):
+#Define class for getting converter material
+
+class SetupConvertOperator(Operator):
 
 
-    #Give the operation a name and label
-    bl_idname = "object.color_operator"
-    bl_label = "Bake Color Map"
+    # Give the operation a name and label
+    bl_idname = "object.converter_operator"
+    bl_label = "Setup Converter"
 
-    #execute it the function
+
     def execute(self, context):
 
-        #Bake Color
-        bpy.context.view_layer.objects.active = obj
-        bpy.ops.object.bake(type='DIFFUSE', save_mode='EXTERNAL')
-        img.save_render(filepath='C:\\Textures\\ColorMap_baked.png')
+        #read from blender file and load necessary objects
+        file_path = r'H:\Main Storage\College\Classes Current\Techincal Pipeline\anim-t380-2022-assignment\Final\Asset\Converter_Setup.blend'
+        inner_path = 'Object'
+        object_name = 'ConvertBakePlane'
+        bpy.ops.wm.append(filepath = os.path.join(file_path, inner_path, object_name), directory = os.path.join(file_path, inner_path), filename = object_name)
 
         return {'FINISHED'}
 
-#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~RoughnessMap
-
-#Define class for Recalculating Normals
-class BakeRoughnessMapOperator(Operator):
 
 
-    #Give the operation a name and label
-    bl_idname = "object.roughness_operator"
-    bl_label = "Bake Roughness Map"
-
-    #execute it the function
-    def execute(self, context):
-
-        #Bake Roughness
-        bpy.context.view_layer.objects.active = obj
-        bpy.ops.object.bake(type='ROUGHNESS', save_mode='EXTERNAL')
-        img.save_render(filepath='C:\\Textures\\RoughnessMap_baked.png')
-
-        return {'FINISHED'}
-
-#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~NormalMap
-
-#Define class for Recalculating Normals
-class BakeNormalMapOperator(Operator):
-
-
-    #Give the operation a name and label
-    bl_idname = "object.normal_operator"
-    bl_label = "Bake Normal Map"
-
-    #execute it the function
-    def execute(self, context):
-
-        #Bake Normal
-        bpy.context.view_layer.objects.active = obj
-        bpy.ops.object.bake(type='NORMAL', save_mode='EXTERNAL')
-        img.save_render(filepath='C:\\Textures\\NormalMap_baked.png')
-
-        return {'FINISHED'}
-
-#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~EmissionMap
-
-#Define class for Recalculating Normals
-class BakeEmissionMapOperator(Operator):
-
-
-    #Give the operation a name and label
-    bl_idname = "object.emission_operator"
-    bl_label = "Bake Emission Map"
-
-    #execute it the function
-    def execute(self, context):
-
-        #Bake Emission
-        bpy.context.view_layer.objects.active = obj
-        bpy.ops.object.bake(type='EMIT', save_mode='EXTERNAL')
-        img.save_render(filepath='C:\\Textures\\Emission_baked.png')
-
-        return {'FINISHED'}
-
-#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~AmbientOcclusionMap
-
-#Define class for Recalculating Normals
-class BakeAoMapOperator(Operator):
-
-
-    #Give the operation a name and label
-    bl_idname = "object.ao_operator"
-    bl_label = "Bake AO Map"
-
-    #execute it the function
-    def execute(self, context):
-
-        #Bake Ambient Occlusion
-        bpy.context.view_layer.objects.active = obj
-        bpy.ops.object.bake(type='AO', save_mode='EXTERNAL')
-        img.save_render(filepath='C:\\Textures\\AO_baked.png')
-
-        return {'FINISHED'}
 
 
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~Bake All Maps
 
-#Define class for Bakign the ARM Map:
+#Define class for Baking the ARM Map:
 class BakeARMOperator(Operator):
 
 
     #Give the operation a name and label
-    bl_idname = "object.all_operator"
+    bl_idname = "object.bake_operator"
     bl_label = "Bake All Maps"
 
     #execute it the function
     def execute(self, context):
+        obj = bpy.context.active_object
+        # Set a default name and Texture size
+        image_name = obj.name + 'ARM_BakedTexture'
 
-        #Bake Ambient Occlusion
+        img = bpy.data.images.new(image_name, 1024, 1024)
+
+        # Since there are multiple material, we need to iterate on the materials to assign them to
+        # the correct maps
+
+        for mat in obj.data.materials:
+            mat.use_nodes = True
+            # Because this was made with node we say its true
+            nodes = mat.node_tree.nodes
+            texture_node = nodes.new('ShaderNodeTexImage')
+            texture_node.name = 'Bake_node'
+            texture_node.select = True
+            nodes.active = texture_node
+
+            # Assign an image to the node
+
+            texture_node.image = img
+
+        #Bake ARM
+
         bpy.context.view_layer.objects.active = obj
 
         bpy.ops.object.bake(type='DIFFUSE', save_mode='EXTERNAL')
-        img.save_render(filepath='C:\\Textures\\ColorMap_baked.png')
+        img.save_render(filepath= r'C:\Users\vince\Downloads\ARMMap_baked.png')
 
 
         return {'FINISHED'}
@@ -182,7 +107,7 @@ class BakeARMOperator(Operator):
 class QuickUI(bpy.types.Panel):
 
     bl_idname = 'VIEW3D_PT_T_panel'
-    bl_label = 'Bake Textures'
+    bl_label = 'Convert and Bake ARM Texture'
     bl_space_type = 'VIEW_3D'
     bl_region_type = 'UI'
 
@@ -196,7 +121,7 @@ class QuickUI(bpy.types.Panel):
 
 
         #Get each operation that will be used for the panel
-        col.operator(BakeColorMapOperator.bl_idname, text="Setup Convert")
+        col.operator(SetupConvertOperator.bl_idname, text="Setup Convert")
         col.operator(BakeColorMapOperator.bl_idname, text="Bake Color Map")
 
 
@@ -204,7 +129,7 @@ class QuickUI(bpy.types.Panel):
 
 # Register and Unregister the operations used in the panel
 
-classes = (BakeColorMapOperator, SetupConvertOperator, QuickUI)
+classes = (SetupConvertOperator, BakeARMOperator, QuickUI)
 
 def register():
     from bpy.utils import register_class
